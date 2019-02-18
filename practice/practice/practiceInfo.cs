a7e5136ef2afbf46143b87c19151093e71b09f07
synnestenvold <46318588+synnestenvold@users.noreply.github.com>
2019-02-18 15:24:11 +0100

    Add files via upload

---
+++ practice/practice/practiceInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace practice
+{
+    public class practiceInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "practice";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("cc69f8d1-7e1f-4c66-be35-93dc6f354592");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
