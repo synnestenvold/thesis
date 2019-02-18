9fccf9e37a23515c1a98314cc1f8c539bced5dff
synnestenvold <synne@spacemaker.ai>
2019-02-18 15:50:36 +0100

    cleaning

--- practice/practice/practiceInfo.cs
+++ practice/practice/practiceInfo.cs
@@ -18,7 +18,6 @@ namespace practice
         {
             get
             {
-                //yoyoyo
                 //Return a 24x24 pixel bitmap to represent this GHA library.
                 return null;
             }
@@ -57,62 +56,3 @@ namespace practice
         }
     }
 }
-=======
-﻿using System;
-using System.Drawing;
-using Grasshopper.Kernel;
-
-namespace practice
-{
-    public class practiceInfo : GH_AssemblyInfo
-    {
-        public override string Name
-        {
-            get
-            {
-                return "practice";
-            }
-        }
-        public override Bitmap Icon
-        {
-            get
-            {
-                //Return a 24x24 pixel bitmap to represent this GHA library.
-                return null;
-            }
-        }
-        public override string Description
-        {
-            get
-            {
-                //Return a short string describing the purpose of this GHA library.
-                return "";
-            }
-        }
-        public override Guid Id
-        {
-            get
-            {
-                return new Guid("cc69f8d1-7e1f-4c66-be35-93dc6f354592");
-            }
-        }
-
-        public override string AuthorName
-        {
-            get
-            {
-                //Return a string identifying you or your company.
-                return "";
-            }
-        }
-        public override string AuthorContact
-        {
-            get
-            {
-                //Return a string representing your preferred contact details.
-                return "";
-            }
-        }
-    }
-}
->>>>>>> a7e5136ef2afbf46143b87c19151093e71b09f07
