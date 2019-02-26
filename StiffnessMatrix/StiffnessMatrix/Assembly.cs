03a8425eee68700669ced838d0dd56b8518a002d
Martinvee9 <martinvee@hotmail.com>
2019-02-26 15:35:44 +0100

    Added assembly of stiffness matrix to FEMeshedBrep

--- StiffnessMatrix/StiffnessMatrix/Assembly.cs
+++ StiffnessMatrix/StiffnessMatrix/Assembly.cs
@@ -11,53 +11,42 @@ namespace StiffnessMatrix
     class Assembly
     {
 
-        public Matrix<double> assemblyMatrix(Matrix<double> Ke)
+        public Matrix<double> assemblyMatrix(Matrix<double> Ke, int[] C_)
         {
-            //double[,] array = new double[6, 4];
-            //array = fillZeros(array);
-
+           
             Matrix<double> K = Matrix<double>.Build.Dense(81, 81);
+            int[] C = C_;
 
-            int [,] C = new int[,]
-            
+
+            for (int i = 0; i<C.Length; i++)
             {
-                {1,2,9,8,19,11,18,17}
-
-            };
-
-            for (int i = 0; i<C.GetLength(1); i++)
-            {
-                for(int j = 0; j<C.GetLength(1); j++)
+                for(int j = 0; j<C.Length; j++)
                 {
-                    //Console.WriteLine("i,j :" + i + " " + j);
 
+                    //int a = C[i];
+                    //int b = C[j];
 
-                    int a = C[0, i] - 1;
-                    int b = C[0, j] - 1;
+                    //Console.WriteLine("Ci: " + a + " Cj: " + b);
 
-                    //Console.WriteLine("a,b "+ a + " " + b);
-
+                    //Inserting 3x3 stiffness matri
                     for (int k = 0; k < 3; k++)
                     {
                         for (int e = 0; e<3; e++)
                         {
-                            int c = 3 * a + k;
-                            int d = 3 * b + e;
-
-                            int f = 3 * i + k;
-                            int g = 3 * j + e;
-
-                            //Console.WriteLine("K :" + c + " " + d);
-                            //Console.WriteLine("Ke :" + f+ " " +g) ;
-                            //Console.WriteLine("");
-                            K[3 * a + k, 3 * b + e] = Ke[3 * i + k, 3 * j + e];
+                            K[3 * C[i] + k, 3 * C[j] + e] = Ke[3 * i + k, 3 * j + e];
+                            //Console.WriteLine("Ki: " + (3 * a + k) + " Kj: " + (3 *b + e) + " Kei: " + (3 * i + k) + " Kej: " + (3 * j + e)); 
                         }
                     }
                 }
             }
 
+
+            
             for (int i = 0; i < K.RowCount; i++)
+
             {
+                
+                Console.WriteLine("Row: " + (i+1));
                 for (int j = 0; j < K.ColumnCount; j++)
                 {
                     Console.Write("{0:0.00}", K[i, j]);
@@ -67,11 +56,9 @@ namespace StiffnessMatrix
             }
             Console.WriteLine();
 
-       
-
             Console.ReadKey();
             return K;
         }
 
-        }
+    }
 }
