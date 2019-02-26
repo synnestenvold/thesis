1324f9a4dffd318a73fa7e85b608485c1c2aeab1
synnestenvold <synne@spacemaker.ai>
2019-02-26 12:55:06 +0100

    Converting to global nodes

--- MeshBox/MeshBox/MeshBoxComponent.cs
+++ MeshBox/MeshBox/MeshBoxComponent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
-//using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra;
 
 // In order to load the result of this wizard, you will also need to
 // add the output bin/ folder of this project to the list of loaded
@@ -14,13 +14,7 @@ namespace MeshBox
 {
     public class MeshBoxComponent : GH_Component
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
-        /// </summary>
+  
         public MeshBoxComponent()
           : base("MeshBox", "MeshB",
               "Description",
@@ -37,7 +31,9 @@ namespace MeshBox
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddBrepParameter("Breps", "B", "List of new Breps", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Breps", "B", "List of new Breps", GH_ParamAccess.list);
+            //pManager.AddNumberParameter("Connectivity", "C", "Connectivity matrix", GH_ParamAccess.list);
+
         }
 
         /// <summary>
@@ -53,6 +49,8 @@ namespace MeshBox
             double u = 1;
             double v = 1;
             double w = 1;
+            
+       
 
             // Then we need to access the input parameters individually. 
             // When data cannot be extracted from a parameter, we should abort this method.
@@ -61,18 +59,20 @@ namespace MeshBox
             if (!DA.GetData(2, ref v)) return;
             if (!DA.GetData(3, ref w)) return;
 
+            //Point3d[] global_nodes = brp.DuplicateVertices();
+
             if (u < 1 || v < 1 || w < 1)
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "One of the input parameters is less than one.");
                 return;
             }
 
-            List<Brep> breps = CreateNewBreps(brp, u, v, w);
+            List<List<int>> global_numbering = CreateNewBreps(brp, u, v, w);
 
-            DA.SetDataList(0, breps);
+            //DA.SetDataList(0, global_nodes);
         }
 
-        private List<Brep> CreateNewBreps(Brep brp, double u, double v, double w)
+        private List<List<int>> CreateNewBreps(Brep brp, double u, double v, double w)
         {
             Point3d[] nodes = brp.DuplicateVertices();
             double lx = nodes[0].DistanceTo(nodes[1]);
@@ -83,23 +83,49 @@ namespace MeshBox
             double ly_new = lx / v;
             double lz_new = lx / w;
             List<Brep> breps = new List<Brep>();
+            List<Point3d> global_nodes = new List<Point3d>();
 
-            for (int i = 0; i < u; i++)
+            for (int k = 0; k <= u; k++)
             {
-                for (int j = 0; j < v; j++)
+                for (int j = 0; j <= v; j++)
                 {
-                    for (int k = 0; k < w; k++)
+                    for (int i = 0; i <= w; i++)
                     {
-                        Interval x = new Interval(lx_new*i, lx_new*(i+1));
-                        Interval y = new Interval(ly_new*j, ly_new*(j+1));
-                        Interval z = new Interval(lz_new*k, lz_new*(k+1));
-                        Box box_new = new Box(Plane.WorldXY, x, y, z);
-                        Brep brep_new = box_new.ToBrep();
-                        breps.Add(brep_new);
+                        if (i < u && j < v && k < w)
+                        {
+                            Interval x = new Interval(lx_new * i, lx_new * (i + 1));
+                            Interval y = new Interval(ly_new * j, ly_new * (j + 1));
+                            Interval z = new Interval(lz_new * k, lz_new * (k + 1));
+                            Box box_new = new Box(Plane.WorldXY, x, y, z);
+                            Brep brep_new = box_new.ToBrep();
+                            breps.Add(brep_new); //Adds the smaller breps to the list
+                        }
+                        
+                        Point3d node = new Point3d(lx_new*i, ly_new*j, lz_new*k);
+                        global_nodes.Add(node); //Adds each point to the list of global nodes
                     }
                 }
             }
-            return breps;
+            List<List<int>> global_numbering = new List<List<int>>();
+
+            for (int b = 0; b< breps.Count; b++) //for each smaller brep...
+            {
+                Point3d[] brep_nodes = breps[b].DuplicateVertices();
+                List<int> brep_numbering = new List<int>();
+                for (int n = 0; n < brep_nodes.Length; n++) //and each node in the smaller brep...
+                {
+                    for (int m = 0; m < global_nodes.Count; m++)
+                    {
+                        if (brep_nodes[n].Equals(global_nodes[m])) //compare the point to the global node list
+                        {
+                            brep_numbering.Add(m + 1); //and add the global node-index to the list
+                        }
+                    }
+                }
+                global_numbering.Add(brep_numbering); //putting all the lists in a list
+            }
+
+            return global_numbering;
         }
 
         /// <summary>
