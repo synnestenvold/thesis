fd9997d3f0b3e2a3bfdb1637401b083f8b74eb82
synnestenvold <synne@spacemaker.ai>
2019-02-27 16:20:16 +0100

    Small fix

--- FEMeshedBrep/FEMeshedBrep/FEMeshedBrepComponent.cs
+++ FEMeshedBrep/FEMeshedBrep/FEMeshedBrepComponent.cs
@@ -69,14 +69,11 @@ namespace FEMeshedBrep
             {
                 List<GH_Integer> connectedNodes = (List<GH_Integer>)tree.get_Branch(i);
         
-
                 K_i = aSM.assemblyMatrix(K_e, connectedNodes);
 
                 K_tot = K_tot + K_i;
                 
             }
-            
-
             
            
             Matrix<double> K_tot_inverse = K_tot.Inverse();
@@ -99,9 +96,7 @@ namespace FEMeshedBrep
             Cmatrix C_new = new Cmatrix(10, 0.3);
             Matrix<double> C = C_new.CreateMatrix();
             Vector<double> stress = C.Multiply(strain);
-            //TODO: Fix tree structure, we want a list with 3 components: three deformations in a list, stress and strain.
-            //List<List<double>> list = new List<List<double>> { u1, { 5 }, { 5 } };
-            //DA.SetDataList(0, u1);
+     
             DA.SetDataList(0, u);
             DA.SetDataList(1, strain);
             DA.SetDataList(2, stress);
