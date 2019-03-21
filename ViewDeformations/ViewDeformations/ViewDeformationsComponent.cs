a3468a60d73727dea74a3c16442337a3a71a2650
synnestenvold <synne@spacemaker.ai>
2019-03-21 18:02:17 +0100

    Creating basis for deformation view

---
+++ ViewDeformations/ViewDeformations/ViewDeformationsComponent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+
+
+namespace ViewDeformations
+{
+    public class ViewDeformationsComponent : GH_Component
+    {
+     
+        public ViewDeformationsComponent()
+          : base("ViewDeformations", "ViewDef",
+              "Description",
+              "Category3", "Preview")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points for Breps", "P", "Breps in coordinates", GH_ParamAccess.tree);
+            pManager.AddNumberParameter("Displacement", "Disp", "Displacement in each dof", GH_ParamAccess.list);
+
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points for new Breps", "P", "Breps in coordinates", GH_ParamAccess.tree);
+
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+ 
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("0391f003-161b-49e1-931b-71ca89c69aa6"); }
+        }
+    }
+}
