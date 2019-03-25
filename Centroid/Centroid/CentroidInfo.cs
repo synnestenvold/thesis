51816ba16f4aa6e17f299cd42c5d2eb80518dfd6
Martinvee9 <martinvee@hotmail.com>
2019-03-25 12:29:03 +0100

    Centroid

---
+++ Centroid/Centroid/CentroidInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Centroid
+{
+    public class CentroidInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Centroid";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("2ecdc1f4-46a3-4083-b778-a19ae722066d");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "HP Inc.";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
