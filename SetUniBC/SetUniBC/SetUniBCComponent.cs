acc55642c741b1be9625ffc43b46d254ab2a7a34
Martinvee9 <martinvee@hotmail.com>
2019-03-29 18:07:36 +0100

    Changing to cones for BC

--- SetUniBC/SetUniBC/SetUniBCComponent.cs
+++ SetUniBC/SetUniBC/SetUniBCComponent.cs
@@ -38,7 +38,8 @@ namespace SetUniBC
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddTextParameter("BC points", "BC", "BC in point, (x,y,z);(Fx,Fy,Fz)", GH_ParamAccess.list);
-            pManager.AddLineParameter("BC-arrows", "L", "Arrows showing the boundary conditions", GH_ParamAccess.list);
+            pManager.AddBrepParameter("BC-cones", "C", "Cones showing the boundary conditions", GH_ParamAccess.list);
+
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
@@ -120,23 +121,22 @@ namespace SetUniBC
                 pointsBC.Add(s + ";" + restrains);
             }
 
-            List<Line> arrows = DrawBC(pointsBC);
+            List<Brep> cones = DrawBC(pointsBC);
 
             
 
             DA.SetDataList(0, pointsBC);
-            DA.SetDataList(1, arrows);
+            DA.SetDataList(1, cones);
         }
 
-        public List<Line> DrawBC(List<string> pointsBC)
+        public List<Brep> DrawBC(List<string> pointsBC)
         {
             List<Line> arrows = new List<Line>();
 
             List<double> BCPoints = new List<double>();
             List<double> restrains = new List<double>();
 
-            double BCRef = 1;
-            double arrowRef = 0.2;
+            List<Brep> bcCones = new List<Brep>();
 
             foreach (string s in pointsBC)
             {
@@ -160,46 +160,24 @@ namespace SetUniBC
                 Point3d arrowPart2 = new Point3d(0, 0, 0);
                 Point3d endPoint = new Point3d(0, 0, 0);
 
-                if (restrain1 == 0)
-                {
-                    endPoint = new Point3d(BCPoints1 + BCRef, BCPoints2, BCPoints3);
+                double height = 0.2;
+                double radius = 0.2;
 
-                    arrowPart1 = new Point3d(BCPoints1 + BCRef - arrowRef, BCPoints2 - arrowRef, BCPoints3);
-                    arrowPart2 = new Point3d(BCPoints1 + BCRef - arrowRef, BCPoints2 + arrowRef, BCPoints3);
-                    
+                Point3d p1 = new Point3d(BCPoints1, BCPoints2, BCPoints3);
+                Point3d p2 = Point3d.Add(p1, new Point3d(0, 1, 0));
+                Point3d p3 = Point3d.Add(p1, new Point3d(1, 0, 0));
 
-                    arrows.Add(new Line(startPoint, endPoint));
-                    arrows.Add(new Line(endPoint, arrowPart1));
-                    arrows.Add(new Line(endPoint, arrowPart2));
-                }
+                Plane bcPlane = new Plane(p1, p2, p3);
 
-                if (restrain2 == 0)
-                {
-                    endPoint = new Point3d(BCPoints1, BCPoints2 + BCRef, BCPoints3);
+                Cone cone = new Cone(bcPlane, height, radius);
 
-                    arrowPart1 = new Point3d(BCPoints1 - arrowRef, BCPoints2 + BCRef - arrowRef, BCPoints3);
-                    arrowPart2 = new Point3d(BCPoints1 + arrowRef, BCPoints2 + BCRef - arrowRef, BCPoints3);
+                Brep brep = cone.ToBrep(true);
 
-                    arrows.Add(new Line(startPoint, endPoint));
-                    arrows.Add(new Line(endPoint, arrowPart1));
-                    arrows.Add(new Line(endPoint, arrowPart2));
-                }
-
-                if (restrain3 == 0)
-                {
-                    endPoint = new Point3d(BCPoints1, BCPoints2, BCPoints3 + BCRef);
-
-                    arrowPart1 = new Point3d(BCPoints1 - arrowRef, BCPoints2 , BCPoints3 + BCRef - arrowRef);
-                    arrowPart2 = new Point3d(BCPoints1 + arrowRef, BCPoints2 , BCPoints3 + BCRef - arrowRef);
-
-                    arrows.Add(new Line(startPoint, endPoint));
-                    arrows.Add(new Line(endPoint, arrowPart1));
-                    arrows.Add(new Line(endPoint, arrowPart2));
-                }
+                bcCones.Add(brep);
 
             }
 
-            return arrows;
+            return bcCones;
         }
 
 
