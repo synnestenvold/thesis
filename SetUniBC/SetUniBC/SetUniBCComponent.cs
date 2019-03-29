596d36a0899017c5de8a5b86d3c46a24c66d44ea
Martinvee9 <martinvee@hotmail.com>
2019-03-29 10:30:18 +0100

    Creating BC-symbols

--- SetUniBC/SetUniBC/SetUniBCComponent.cs
+++ SetUniBC/SetUniBC/SetUniBCComponent.cs
@@ -38,6 +38,7 @@ namespace SetUniBC
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddTextParameter("BC points", "BC", "BC in point, (x,y,z);(Fx,Fy,Fz)", GH_ParamAccess.list);
+            pManager.AddLineParameter("BC-arrows", "L", "Arrows showing the boundary conditions", GH_ParamAccess.list);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
@@ -119,10 +120,89 @@ namespace SetUniBC
                 pointsBC.Add(s + ";" + restrains);
             }
 
+            List<Line> arrows = DrawBC(pointsBC);
+
+            
+
             DA.SetDataList(0, pointsBC);
+            DA.SetDataList(1, arrows);
         }
 
-    
+        public List<Line> DrawBC(List<string> pointsBC)
+        {
+            List<Line> arrows = new List<Line>();
+
+            List<double> BCPoints = new List<double>();
+            List<double> restrains = new List<double>();
+
+            double BCRef = 1;
+            double arrowRef = 0.2;
+
+            foreach (string s in pointsBC)
+            {
+                string coordinate = (s.Split(';'))[0];
+                string iBC = (s.Split(';'))[1];
+
+                string[] coord = (coordinate.Split(','));
+                string[] iBCs = (iBC.Split(','));
+
+
+                double BCPoints1 = Math.Round(double.Parse(coord[0]), 8);
+                double BCPoints2 = Math.Round(double.Parse(coord[1]), 8);
+                double BCPoints3 = Math.Round(double.Parse(coord[2]), 8);
+
+                double restrain1 = Math.Round(double.Parse(iBCs[0]), 8);
+                double restrain2 = Math.Round(double.Parse(iBCs[1]), 8);
+                double restrain3 = Math.Round(double.Parse(iBCs[2]), 8);
+
+                Point3d startPoint = new Point3d(BCPoints1, BCPoints2, BCPoints3);
+                Point3d arrowPart1 = new Point3d(0, 0, 0);
+                Point3d arrowPart2 = new Point3d(0, 0, 0);
+                Point3d endPoint = new Point3d(0, 0, 0);
+
+                if (restrain1 == 0)
+                {
+                    endPoint = new Point3d(BCPoints1 + BCRef, BCPoints2, BCPoints3);
+
+                    arrowPart1 = new Point3d(BCPoints1 + BCRef - arrowRef, BCPoints2 - arrowRef, BCPoints3);
+                    arrowPart2 = new Point3d(BCPoints1 + BCRef - arrowRef, BCPoints2 + arrowRef, BCPoints3);
+                    
+
+                    arrows.Add(new Line(startPoint, endPoint));
+                    arrows.Add(new Line(endPoint, arrowPart1));
+                    arrows.Add(new Line(endPoint, arrowPart2));
+                }
+
+                if (restrain2 == 0)
+                {
+                    endPoint = new Point3d(BCPoints1, BCPoints2 + BCRef, BCPoints3);
+
+                    arrowPart1 = new Point3d(BCPoints1 - arrowRef, BCPoints2 + BCRef - arrowRef, BCPoints3);
+                    arrowPart2 = new Point3d(BCPoints1 + arrowRef, BCPoints2 + BCRef - arrowRef, BCPoints3);
+
+                    arrows.Add(new Line(startPoint, endPoint));
+                    arrows.Add(new Line(endPoint, arrowPart1));
+                    arrows.Add(new Line(endPoint, arrowPart2));
+                }
+
+                if (restrain3 == 0)
+                {
+                    endPoint = new Point3d(BCPoints1, BCPoints2, BCPoints3 + BCRef);
+
+                    arrowPart1 = new Point3d(BCPoints1 - arrowRef, BCPoints2 , BCPoints3 + BCRef - arrowRef);
+                    arrowPart2 = new Point3d(BCPoints1 + arrowRef, BCPoints2 , BCPoints3 + BCRef - arrowRef);
+
+                    arrows.Add(new Line(startPoint, endPoint));
+                    arrows.Add(new Line(endPoint, arrowPart1));
+                    arrows.Add(new Line(endPoint, arrowPart2));
+                }
+
+            }
+
+            return arrows;
+        }
+
+
         protected override System.Drawing.Bitmap Icon
         {
             get
