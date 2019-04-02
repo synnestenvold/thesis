8dcf983754e2cf25aa9b61cc067a104e41c6a947
synnestenvold <synne@spacemaker.ai>
2019-04-02 10:20:41 +0200

    Changing plane

--- DeformationSlider/DeformationSlider/DeformationSliderComponent.cs
+++ DeformationSlider/DeformationSlider/DeformationSliderComponent.cs
@@ -65,7 +65,7 @@ namespace DeformationSlider
             textValue.Text = "Scale size: " + Math.Round(length).ToString();
             Point3d end = curve.PointAtEnd;
             Point3d p0 = Point3d.Add(end, new Point3d(0, 0, 2*refSize));
-            Point3d p1 = Point3d.Add(end, new Point3d(0, -1, 2*refSize));
+            Point3d p1 = Point3d.Add(end, new Point3d(0, 1, 2*refSize));
             Point3d p2 = Point3d.Add(end, new Point3d(0, 0, 1+2*refSize));
             textValue.TextPlane = new Plane(p0, p1, p2);
             textValue.Height = refSize;
