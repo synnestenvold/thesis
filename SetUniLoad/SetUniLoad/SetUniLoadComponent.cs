47a5bc4ae260394449c8270e686b08b5e0bc3900
Martinvee9 <martinvee@hotmail.com>
2019-04-02 15:25:07 +0200

    BC and Load relative

--- SetUniLoad/SetUniLoad/SetUniLoadComponent.cs
+++ SetUniLoad/SetUniLoad/SetUniLoadComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
+using System.Drawing;
 
 // In order to load the result of this wizard, you will also need to
 // add the output bin/ folder of this project to the list of loaded
@@ -32,16 +33,18 @@ namespace SetUniLoad
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddVectorParameter("Force vector", "V", "Direction and load amount in kN/m", GH_ParamAccess.item);
             pManager.AddBrepParameter("Surface", "S", "Surface for loading", GH_ParamAccess.item);
+            pManager.AddVectorParameter("Force vector", "V", "Direction and load amount in kN/m", GH_ParamAccess.item);
             pManager.AddIntegerParameter("u-divisions", "U", "U-division", GH_ParamAccess.item);
             pManager.AddIntegerParameter("v-divisions", "V", "V-division", GH_ParamAccess.item);
+            pManager.AddBrepParameter("Brep", "B", "Brep as a reference size", GH_ParamAccess.item);
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddTextParameter("Loaded points", "LP", "Loads in point, (x,y,z);(Fx,Fy,Fz)", GH_ParamAccess.list);
-            pManager.AddBrepParameter("Load-arrows", "L", "Arrows showing the load", GH_ParamAccess.list);
+            pManager.AddBrepParameter("Load-arrows", "Geometry", "Arrows showing the load", GH_ParamAccess.list);
+            pManager.AddColourParameter("Arrow coloring", "Color", "Coloring of arrows", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -52,146 +55,41 @@ namespace SetUniLoad
         protected override void SolveInstance(IGH_DataAccess DA)
         {
 
+            Brep surface = new Brep();
             Vector3d forceVec = new Vector3d();
-            Brep brep = new Brep();
             int u = 2;
             int v = 2;
-
-            Point3d[] vertices = new Point3d[4];
-
-
-            if (!DA.GetData(0, ref forceVec)) return;
-            if (!DA.GetData(1, ref brep)) return;
-            if (!DA.GetData(2, ref u)) return;
-            if (!DA.GetData(3, ref v)) return;
-
-            vertices = brep.DuplicateVertices();
-
-            //FINDING CORNER POINTS
-            List<Point3d> cornerPoints = new List<Point3d>();
-            cornerPoints.Add(vertices[0]);
-            cornerPoints.Add(vertices[1]);
-            cornerPoints.Add(vertices[2]);
-            cornerPoints.Add(vertices[3]);
-
-            double l_u1 = vertices[0].DistanceTo(vertices[1])/u;
-            double l_u2 = vertices[3].DistanceTo(vertices[2])/u;
-
-            double l_v1 = vertices[0].DistanceTo(vertices[3])/v;
-            double l_v2 = vertices[1].DistanceTo(vertices[2])/v;
-
-            Vector3d vec_u1 = (vertices[1] - vertices[0]) / vertices[0].DistanceTo(vertices[1]);
-            Vector3d vec_u2 = (vertices[2] - vertices[3]) / vertices[3].DistanceTo(vertices[2]);
-
-            Vector3d vec_v1 = (vertices[3] - vertices[0]) / vertices[0].DistanceTo(vertices[3]);
-            Vector3d vec_v2 = (vertices[2] - vertices[1]) / vertices[1].DistanceTo(vertices[2]);
-
-
-            //FINDING LINE POINTS
-            List<Point3d> linePoints = new List<Point3d>();
-
-            for(int i = 1; i < u; i++)
-            {
-                Point3d p1 = new Point3d(vertices[0].X + l_u1 * i * vec_u1.X, vertices[0].Y + l_u1 * vec_u1.Y * i, vertices[0].Z + l_u1 * vec_u1.Z * i);
-                linePoints.Add(p1);
-                Point3d p2 = new Point3d(vertices[3].X + l_u2 * i * vec_u2.X, vertices[3].Y + l_u2 * vec_u2.Y * i, vertices[3].Z + l_u2 * vec_u2.Z * i);
-                linePoints.Add(p2);
-            }
-
-            for (int i = 1; i < v; i++)
-            {
-                Point3d p1 = new Point3d(vertices[0].X + l_v1 * i * vec_v1.X, vertices[0].Y + l_v1 * vec_v1.Y * i, vertices[0].Z + l_v1 * vec_v1.Z * i);
-                linePoints.Add(p1);
-                Point3d p2 = new Point3d(vertices[1].X + l_v2 * i * vec_v2.X, vertices[1].Y + l_v2 * vec_v2.Y * i, vertices[1].Z + l_v2 * vec_v2.Z * i);
-                linePoints.Add(p2);
-            }
-
-
-            //FINDING CENTER POINTS
-            List<Point3d> centerPoints = new List<Point3d>();
-
-            for(int i = 1; i <u; i++)
-            {
-                Point3d p1_u = new Point3d(vertices[0].X + l_u1 * i * vec_u1.X, vertices[0].Y + l_u1 * vec_u1.Y * i, vertices[0].Z + l_u1 * vec_u1.Z * i);
-                Point3d p2_u = new Point3d(vertices[3].X + l_u2 * i * vec_u2.X, vertices[3].Y + l_u2 * vec_u2.Y * i, vertices[3].Z + l_u2 * vec_u2.Z * i);
-
-                Vector3d vec_u = (p2_u - p1_u) / (p1_u.DistanceTo(p2_u));
-
-                Double length_u1 = p1_u.DistanceTo(p2_u) / u;
-
-                for (int j = 1; j < v; j++)
-                {
-                    Point3d p1_v = new Point3d(p1_u.X + length_u1 * j * vec_u.X, p1_u.Y + length_u1 * j * vec_u.Y, p1_u.Z + length_u1 * j * vec_u.Z);
-                    centerPoints.Add(p1_v);
-                }
-            }
-
-            //DISTRIBUTING LOAD TO POINTS FOUND
-            double pointsCount = 4*centerPoints.Count + cornerPoints.Count + 2*linePoints.Count;
-            double area = brep.GetArea();
-            forceVec = forceVec * area;
-
-            List<string> centerPointsString = new List<string>();
-            List<string> linePointsString = new List<string>();
-            List<string> cornerPointsString = new List<string>();
-
-            List<string> pointLoads = new List<string>();
+            Brep origBrep = new Brep();
 
             
-            //Center
-            foreach (Point3d p in centerPoints)
-            {
-                string pointString = p.X.ToString() + "," + p.Y.ToString() + "," + p.Z.ToString();
-                centerPointsString.Add(pointString);
-            }
-            string centerVector = 4*(forceVec.X)/pointsCount + "," + 4*(forceVec.Y)/pointsCount + "," + 4*(forceVec.Z)/pointsCount;
+            if (!DA.GetData(0, ref surface)) return;
+            if (!DA.GetData(1, ref forceVec)) return;
+            if (!DA.GetData(2, ref u)) return;
+            if (!DA.GetData(3, ref v)) return;
+            if (!DA.GetData(4, ref origBrep)) return;
 
-            List<string> centerPointLoads = new List<string>();
-            foreach (string s in centerPointsString)
-            {
-                centerPointLoads.Add(s + ";" + centerVector);
-            }
+            List<string> pointLoads = FindPointLoads(surface, forceVec, u, v);
 
-            //Line
-            foreach (Point3d p in linePoints)
-            {
-                string pointString = p.X.ToString() + "," + p.Y.ToString() + "," + p.Z.ToString();
-                linePointsString.Add(pointString);
-            }
-            string lineVector = 2 * (forceVec.X) / pointsCount + "," + 2 * (forceVec.Y) / pointsCount + "," + 2 * (forceVec.Z) / pointsCount;
+            ///////FOR PREVIEWING OF LOADS///////
 
-            List<string> linePointLoads = new List<string>();
-            foreach (string s in linePointsString)
-            {
-                linePointLoads.Add(s + ";" + lineVector);
-            }
+            //Setting up values for reflength and angle for rotation of area
+            VolumeMassProperties vmp = VolumeMassProperties.Compute(origBrep);
+            Point3d centroid = vmp.Centroid;
+            double volume = origBrep.GetVolume();
+            double sqrt3 = (double)1 / 3;
+            double refLength = Math.Pow(volume, sqrt3);
 
-            //Corner
-            foreach (Point3d p in cornerPoints)
-            {
-                string pointString = p.X.ToString() + "," + p.Y.ToString() + "," + p.Z.ToString();
-                cornerPointsString.Add(pointString);
-            }
-            string cornerVector = (forceVec.X) / pointsCount + "," + (forceVec.Y) / pointsCount + "," + (forceVec.Z) / pointsCount;
+            List<Brep> arrows = DrawLoads(pointLoads, refLength);
 
-            List<string> cornerPointLoads = new List<string>();
-            foreach (string s in cornerPointsString)
-            {
-                cornerPointLoads.Add(s + ";" + cornerVector);
-            }
-
-            pointLoads.AddRange(centerPointLoads);
-            pointLoads.AddRange(linePointLoads);
-            pointLoads.AddRange(cornerPointLoads);
-
-            List<Brep> arrows = DrawLoads(pointLoads);
+            Color color = Color.Blue;
 
             DA.SetDataList(0, pointLoads);
             DA.SetDataList(1, arrows);
+            DA.SetData(2, color);
 
         }
 
-        public List<Brep> DrawLoads(List<string> pointLoads)
+        public List<Brep> DrawLoads(List<string> pointLoads, double refLength)
         {
 
             List<Brep> arrows = new List<Brep>();
@@ -199,8 +97,8 @@ namespace SetUniLoad
             List<double> loadCoord = new List<double>();
             List<double> pointValues = new List<double>();
 
-            double arrowHeight = 0.1;
-            double arrowRadius = 0.1;
+            double arrowHeight = (double)refLength / 50;
+            double arrowRadius = (double)refLength / 50;
             double cRadius = 0.01;
 
             foreach (string s in pointLoads)
@@ -339,6 +237,129 @@ namespace SetUniLoad
                 }
             }
             return arrows;
+        }
+
+        public List<string> FindPointLoads(Brep surface, Vector3d forceVec, int u, int v)
+        {
+            Point3d [] vertices = surface.DuplicateVertices();
+
+            //FINDING CORNER POINTS
+            List<Point3d> cornerPoints = new List<Point3d>();
+            cornerPoints.Add(vertices[0]);
+            cornerPoints.Add(vertices[1]);
+            cornerPoints.Add(vertices[2]);
+            cornerPoints.Add(vertices[3]);
+
+            double l_u1 = vertices[0].DistanceTo(vertices[1]) / u;
+            double l_u2 = vertices[3].DistanceTo(vertices[2]) / u;
+
+            double l_v1 = vertices[0].DistanceTo(vertices[3]) / v;
+            double l_v2 = vertices[1].DistanceTo(vertices[2]) / v;
+
+            Vector3d vec_u1 = (vertices[1] - vertices[0]) / vertices[0].DistanceTo(vertices[1]);
+            Vector3d vec_u2 = (vertices[2] - vertices[3]) / vertices[3].DistanceTo(vertices[2]);
+
+            Vector3d vec_v1 = (vertices[3] - vertices[0]) / vertices[0].DistanceTo(vertices[3]);
+            Vector3d vec_v2 = (vertices[2] - vertices[1]) / vertices[1].DistanceTo(vertices[2]);
+
+
+            //FINDING LINE POINTS
+            List<Point3d> linePoints = new List<Point3d>();
+
+            for (int i = 1; i < u; i++)
+            {
+                Point3d p1 = new Point3d(vertices[0].X + l_u1 * i * vec_u1.X, vertices[0].Y + l_u1 * vec_u1.Y * i, vertices[0].Z + l_u1 * vec_u1.Z * i);
+                linePoints.Add(p1);
+                Point3d p2 = new Point3d(vertices[3].X + l_u2 * i * vec_u2.X, vertices[3].Y + l_u2 * vec_u2.Y * i, vertices[3].Z + l_u2 * vec_u2.Z * i);
+                linePoints.Add(p2);
+            }
+
+            for (int i = 1; i < v; i++)
+            {
+                Point3d p1 = new Point3d(vertices[0].X + l_v1 * i * vec_v1.X, vertices[0].Y + l_v1 * vec_v1.Y * i, vertices[0].Z + l_v1 * vec_v1.Z * i);
+                linePoints.Add(p1);
+                Point3d p2 = new Point3d(vertices[1].X + l_v2 * i * vec_v2.X, vertices[1].Y + l_v2 * vec_v2.Y * i, vertices[1].Z + l_v2 * vec_v2.Z * i);
+                linePoints.Add(p2);
+            }
+
+            //FINDING CENTER POINTS
+            List<Point3d> centerPoints = new List<Point3d>();
+
+            for (int i = 1; i < u; i++)
+            {
+                Point3d p1_u = new Point3d(vertices[0].X + l_u1 * i * vec_u1.X, vertices[0].Y + l_u1 * vec_u1.Y * i, vertices[0].Z + l_u1 * vec_u1.Z * i);
+                Point3d p2_u = new Point3d(vertices[3].X + l_u2 * i * vec_u2.X, vertices[3].Y + l_u2 * vec_u2.Y * i, vertices[3].Z + l_u2 * vec_u2.Z * i);
+
+                Vector3d vec_u = (p2_u - p1_u) / (p1_u.DistanceTo(p2_u));
+
+                Double length_u1 = p1_u.DistanceTo(p2_u) / u;
+
+                for (int j = 1; j < v; j++)
+                {
+                    Point3d p1_v = new Point3d(p1_u.X + length_u1 * j * vec_u.X, p1_u.Y + length_u1 * j * vec_u.Y, p1_u.Z + length_u1 * j * vec_u.Z);
+                    centerPoints.Add(p1_v);
+                }
+            }
+
+            //DISTRIBUTING LOAD TO POINTS FOUND
+            double pointsCount = 4 * centerPoints.Count + cornerPoints.Count + 2 * linePoints.Count;
+            double area = surface.GetArea();
+            forceVec = forceVec * area;
+
+            List<string> centerPointsString = new List<string>();
+            List<string> linePointsString = new List<string>();
+            List<string> cornerPointsString = new List<string>();
+
+            List<string> pointLoads = new List<string>();
+
+
+            //Center
+            foreach (Point3d p in centerPoints)
+            {
+                string pointString = p.X.ToString() + "," + p.Y.ToString() + "," + p.Z.ToString();
+                centerPointsString.Add(pointString);
+            }
+            string centerVector = 4 * (forceVec.X) / pointsCount + "," + 4 * (forceVec.Y) / pointsCount + "," + 4 * (forceVec.Z) / pointsCount;
+
+            List<string> centerPointLoads = new List<string>();
+            foreach (string s in centerPointsString)
+            {
+                centerPointLoads.Add(s + ";" + centerVector);
+            }
+
+            //Line
+            foreach (Point3d p in linePoints)
+            {
+                string pointString = p.X.ToString() + "," + p.Y.ToString() + "," + p.Z.ToString();
+                linePointsString.Add(pointString);
+            }
+            string lineVector = 2 * (forceVec.X) / pointsCount + "," + 2 * (forceVec.Y) / pointsCount + "," + 2 * (forceVec.Z) / pointsCount;
+
+            List<string> linePointLoads = new List<string>();
+            foreach (string s in linePointsString)
+            {
+                linePointLoads.Add(s + ";" + lineVector);
+            }
+
+            //Corner
+            foreach (Point3d p in cornerPoints)
+            {
+                string pointString = p.X.ToString() + "," + p.Y.ToString() + "," + p.Z.ToString();
+                cornerPointsString.Add(pointString);
+            }
+            string cornerVector = (forceVec.X) / pointsCount + "," + (forceVec.Y) / pointsCount + "," + (forceVec.Z) / pointsCount;
+
+            List<string> cornerPointLoads = new List<string>();
+            foreach (string s in cornerPointsString)
+            {
+                cornerPointLoads.Add(s + ";" + cornerVector);
+            }
+
+            pointLoads.AddRange(centerPointLoads);
+            pointLoads.AddRange(linePointLoads);
+            pointLoads.AddRange(cornerPointLoads);
+
+            return pointLoads;
         }
 
         protected override System.Drawing.Bitmap Icon
