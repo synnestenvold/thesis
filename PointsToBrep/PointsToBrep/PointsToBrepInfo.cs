f6954d00da160d5907b4448a730bba620c91cdfd
synnestenvold <synne@spacemaker.ai>
2019-04-03 19:15:17 +0200

    Creating brep from points

---
+++ PointsToBrep/PointsToBrep/PointsToBrepInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace PointsToBrep
+{
+    public class PointsToBrepInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "PointsToBrep";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("8661d7b9-0a90-4de5-88b6-2f821e982c9c");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
