de4361d8d45ede22fdb3fcd6b043e31b2673feb3
Martinvee9 <martinvee@hotmail.com>
2019-04-09 11:36:54 +0200

    Update

--- SolidsVR/SolidsVR/MeshCBrep.cs
+++ SolidsVR/SolidsVR/MeshCBrep.cs
@@ -12,9 +12,9 @@ namespace SolidsVR
         /// Initializes a new instance of the MeshCBrep class.
         /// </summary>
         public MeshCBrep()
-          : base("MeshCBrep", "Nickname",
+          : base("MeshCBrep", "MeshCBrep",
               "Description",
-              "Category", "Subcategory")
+              "Category3", "Subcategory3")
         {
         }
 
@@ -23,35 +23,86 @@ namespace SolidsVR
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddBrepParameter("Brep", "B", "Input geometry as a curved brep", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("U count", "U", "Number of divisions in U direction", GH_ParamAccess.item, 1);
+            pManager.AddIntegerParameter("V count", "V", "Number of divisions in V direction", GH_ParamAccess.item, 1);
+            pManager.AddIntegerParameter("W count", "W", "Number of divisions in W direction", GH_ParamAccess.item, 1);
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
+
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            Brep hei = new Brep();
+            pManager.AddGenericParameter("Mesh", "Mesh", "Mesh of Brep", GH_ParamAccess.item);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            // ---variables-- -
+
+            Brep brp = new Brep();
+            int u = 1;
+            int v = 1;
+            int w = 1;
+
+            // --- input ---
+
+            if (!DA.GetData(0, ref brp)) return;
+            if (!DA.GetData(1, ref u)) return;
+            if (!DA.GetData(2, ref v)) return;
+            if (!DA.GetData(3, ref w)) return;
+
+            // --- solve ---
+
+            if (u < 1 || v < 1 || w < 1) //None of the sides can be divided in less than one part
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "One of the input parameters is less than one.");
+                return;
+            }
+
+
+
+            var tuple = CreateNewBreps(nodes, u, v, w); // Getting corner nodes and connectivity matrix
+
+            List<List<Point3d>> elementPoints = tuple.Item1;
+            List<List<int>> connectivity = tuple.Item2;
+            int sizeOfMatrix = 3 * (u + 1) * (v + 1) * (w + 1);
+            Point3d[] globalPoints = CreatePointList(connectivity, elementPoints, sizeOfMatrix);
+
+            //Setting values for Mesh class
+            Mesh_class mesh = new Mesh_class(u, v, w);
+            mesh.SetConnectivity(connectivity);
+            mesh.SetElementPoints(elementPoints);
+            mesh.SetSizeOfMatrix(sizeOfMatrix);
+            mesh.SetGlobalPoints(globalPoints);
+
+            //---output---
+
+            DA.SetData(0, mesh);
         }
 
-        /// <summary>
-        /// Provides an Icon for the component.
-        /// </summary>
-        protected override System.Drawing.Bitmap Icon
+        private Tuple<List<List<Point3d>>, List<List<int>>> CreateNewBreps(Point3d[] nodes, int u, int v, int w)
         {
-            get
-            {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
-            }
+            return null;
+        }
+
+        public Curve[] sortEdges(Curve[] edges)
+        {
+            Curve[] sortedEdges = new Curve[12];
+
+            sortedEdges[0] = edges[5];
+            sortedEdges[1] = edges[7];
+            sortedEdges[2] = edges[3];
+            sortedEdges[3] = edges[1];
+            sortedEdges[4] = edges[4];
+            sortedEdges[5] = edges[6];
+            sortedEdges[6] = edges[2];
+            sortedEdges[7] = edges[0];
+            sortedEdges[8] = edges[11];
+            sortedEdges[9] = edges[10];
+            sortedEdges[10] = edges[9];
+            sortedEdges[11] = edges[8];
+
+            return sortedEdges;
         }
 
         /// <summary>
