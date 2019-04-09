de3d80c36e120a0c1b2fbc2eca4d64514127fd42
Martinvee9 <martinvee@hotmail.com>
2019-04-09 09:59:17 +0200

    Creating new mesh class

---
+++ SolidsVR/SolidsVR/Components/MeshCurveBrep.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace SolidsVR.Components
+{
+    public class MeshCurveBrep : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MeshCurveBrep class.
+        /// </summary>
+        public MeshCurveBrep()
+          : base("MeshCurveBrep", "Nickname",
+              "Description",
+              "Category", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("fa9f6943-0a91-4ffa-a232-6b62e6ec7274"); }
+        }
+    }
+}
