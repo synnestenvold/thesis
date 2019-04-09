5e2459e6d8b39c6e0cd22c5191c5c8c303c02fc0
Martinvee9 <martinvee@hotmail.com>
2019-04-09 18:34:31 +0200

    Creating node and element classes

---
+++ SolidsVR/SolidsVR/Classes/Element.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Rhino.Geometry;
+using System.Linq;
+
+namespace SolidsVR.Classes
+{
+    class Element
+    {
+        List<Point3d> vertices = new List<Point3d>();
+
+        public Element (List<Point3d> _vertices)
+        {
+            vertices = _vertices;
+        }
+    }
+}
---
+++ SolidsVR/SolidsVR/Classes/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Rhino.Geometry;
+using System.Linq;
+
+namespace SolidsVR.Classes
+{
+    class Node
+    {
+        Point3d coordinate;
+
+        public Node (Point3d _coordinate)
+        {
+           coordinate = _coordinate;
+        }
+
+    }
+}
