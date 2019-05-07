b3abc6af3cb310110755217b0ab9797b69080642
synnestenvold <synne@spacemaker.ai>
2019-05-07 20:44:37 +0200

    TopOptComponent created

--- SolidsVR/SolidsVR/Classes/Element.cs
+++ SolidsVR/SolidsVR/Classes/Element.cs
@@ -17,6 +17,7 @@ namespace SolidsVR
         int elementNr = 0;
         Matrix<double> K_e = Matrix<double>.Build.Dense(24, 24);
         List<Matrix<Double>> B_e = new List<Matrix<Double>>();
+        List<double> averageStress = new List<double>();
 
         public Element (List<Node> _vertices, int _elementNr, List<int> _connectivity)
         {
@@ -58,6 +59,25 @@ namespace SolidsVR
         public List<Matrix<double>> GetBMatrixes()
         {
             return B_e;
+        }
+
+        public void SetAverageValuesStress()
+        {
+            averageStress = new List<double>();
+            for (int dir = 0; dir < 7; dir++)
+            {
+                double average = 0;
+                for (int j = 0; j < vertices.Count; j++)
+                {
+                    average += vertices[j].GetStress()[dir];
+                }
+                averageStress.Add(Math.Round(average / vertices.Count, 4));
+            }
+        }
+
+        public double GetAverageStressDir(int dir)
+        {
+            return averageStress[dir];
         }
 
     }
