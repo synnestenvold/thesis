5472cb21fc641533830b2183572153f912343227
Martinvee9 <martinvee@hotmail.com>
2019-05-28 17:06:27 +0200

    Updates

--- SolidsVR/SolidsVR/Components/ViewStresses.cs
+++ SolidsVR/SolidsVR/Components/ViewStresses.cs
@@ -176,7 +176,7 @@ namespace SolidsVR
             else if (dir == 3) rangeValues.Add("S,yz [Mpa]");
             else if (dir == 4) rangeValues.Add("S,xz [Mpa]");
             else if (dir == 5) rangeValues.Add("S,zy [Mpa]");
-            else if (dir == 6) rangeValues.Add("Mises [Mpa]");
+            else if (dir == 6) rangeValues.Add("von Mises [Mpa]");
 
             for (int i = 0; i < breps.Count; i++)
             {
